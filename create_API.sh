21c188f312c6ee5cc4180a5ca6196bd4c58d9516
Juzz Wuzz PC <juzz@crause.com>
2013-01-22 22:16:32 +0200

    Fixed up the python scripts and a bugged makefile.

--- create_API.sh
+++ create_API.sh
@@ -15,6 +15,7 @@ cp -r common_files/* output/$1"_"$2/
 rm -r output/$1"_"$2/Makefile
 cp -r "OS_specific_files/"$2/* output/$1"_"$2/
 rm -r output/$1"_"$2/src/
+mkdir output/$1"_"$2/src/
 cp -r games_src/$1/* output/$1"_"$2/src/
 
 #echo python create_project_files.py "OS_specific_files"$2 $1 output/$1"_"$2/
